9e8b8f6820d003edb11f1e954f3a4405c6c7029a
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-07-08 20:34:05 -0600

    copy to clipboard added

--- src/pages/css/border-effect-page/BorderEffectPage.js
+++ src/pages/css/border-effect-page/BorderEffectPage.js
@@ -1,22 +1,90 @@
-import React, { useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import React, { useRef } from "react";
 import BorderEffect from "../BorderEffect";
 import "./border-effect-page.css";
 
 export default function BorderEffectPage() {
   const navigate = useNavigate();
+  const codeEx1 = `<div class="border-effect-wrapper">
+    <div class="border-effect-box"></div>
+</div>`;
+  const codeEx2 = `.border-effect-wrapper {
+  width: fit-content;
+  border: 1px solid;
+  border-radius: 6px;
+}
 
-  // useEffect(() => {
-  //   const textArea = useRef(null);
+.border-effect-box {
+  width: 212px;
+  height: 88px;
+  position: relative;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 1px 2px 3px 3px rgba(0, 0, 0, 0.4);
+}`;
+  const codeEx3 = `.border-effect-box::before {
+  content: "";
+  width: 200%;
+  height: 400%;
+  position: absolute;
+  left: -50%;
+  top: -150%;
+  background: conic-gradient( blue, aqua);
+  animation: rotate 4s infinite linear;       
+}`;
+  const codeEx4 = `@keyframes rotate {
+  to {
+    transform: rotate(360deg);
+  }
+}`;
+  const codeEx5 = `.border-effect-box::after {
+  content: "Learn how to add border effects";
+  width: 208px;
+  height: 84px;
+  position: relative;
+  top: 2px;
+  left: 2px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  font: 20px sans-serif;
+  color: white;
+  letter-spacing: 2px;
+  text-align: center;
+  background: radial-gradient(blue, black);       
+}`;
 
-  //   function copyToClipboard() {
-  //     const el = textArea.current;
-  //     el.select();
-  //     document.execCommand("copy");
-  //   }
-  // }, []);
-  // ref={textArea} onClick={copyToClipboard()}
-  
+  const copyBtn1Ref = useRef(null);
+  function copyCodeEx1() {
+    const copyBtn1 = copyBtn1Ref.current;
+    navigator.clipboard.writeText(codeEx1);
+    copyBtn1.classList.add("clicked");
+  }
+  const copyBtn2Ref = useRef(null);
+  function copyCodeEx2() {
+    const copyBtn2 = copyBtn2Ref.current;
+    navigator.clipboard.writeText(codeEx2);
+    copyBtn2.classList.add("clicked");
+  }
+  const copyBtn3Ref = useRef(null);
+  function copyCodeEx3() {
+    const copyBtn3 = copyBtn3Ref.current;
+    navigator.clipboard.writeText(codeEx3);
+    copyBtn3.classList.add("clicked");
+  }
+  const copyBtn4Ref = useRef(null);
+  function copyCodeEx4() {
+    const copyBtn4 = copyBtn4Ref.current;
+    navigator.clipboard.writeText(codeEx4);
+    copyBtn4.classList.add("clicked");
+  }
+  const copyBtn5Ref = useRef(null);
+  function copyCodeEx5() {
+    const copyBtn5 = copyBtn5Ref.current;
+    navigator.clipboard.writeText(codeEx5);
+    copyBtn5.classList.add("clicked");
+  }
+
   return (
     <div className="border-effect-page">
       <button className="back-btn" onClick={() => navigate(-1)}>
@@ -30,85 +98,42 @@ export default function BorderEffectPage() {
         <br></br>The second &lt;div&gt; is for the inner box and we will assign it a class name of 'border-effect-box'.
       </p>
       <div className="html-example-container">
-        <pre className="html-example">
-          {`<div class="border-effect-wrapper">
-    <div class="border-effect-box"</div>
-</div>`}
-        </pre>
-        <button className="copy-code-btn" onClick={() => navigator.clipboard.writeText("Hello Universe")}>
-          Copy
-        </button>
+        <pre className="html-example">{codeEx1}</pre>
+        <button className="copy-code-btn copy-btn-1" ref={copyBtn1Ref} onClick={copyCodeEx1}></button>
       </div>
       <p>Everything else is done with CSS. So, we'll start by styling the wrapper and box.</p>
-      <pre className="html-example">
-        {`.border-effect-wrapper {
-    width: fit-content;
-    border: 1px solid;
-    border-radius: 6px;
-}
-
-.border-effect-box {
-    width: 212px;
-    height: 88px;
-    position: relative;
-    border-radius: 5px;
-    overflow: hidden;
-    box-shadow: 1px 2px 3px 3px rgba(0, 0, 0, 0.4);
-}`}
-      </pre>
+      <div className="html-example-container">
+        <pre className="html-example">{codeEx2}</pre>
+        <button className="copy-code-btn copy-btn-2" ref={copyBtn2Ref} onClick={copyCodeEx2}></button>
+      </div>
       <div className="border-ex-1">
         <p>Output:</p>
         <BorderEffect />
       </div>
       <p>Now the fun part —using CSS pseudo-classes. Just a reminder —a pseudo-class duplicates your element. This allows you to make changes to that duplicates' individual styling. It's very useful if you need to layer elements on your page.</p>
-      <pre className="html-example">
-        {`.border-effect-box::before {
-    content: "";
-    width: 200%;
-    height: 400%;
-    position: absolute;
-    left: -50%;
-    top: -150%;
-    background: conic-gradient( blue, aqua);
-    animation: rotate 4s infinite linear;       
-}`}
-      </pre>
+      <div className="html-example-container">
+        <pre className="html-example">{codeEx3}</pre>
+        <button className="copy-code-btn copy-btn-3" ref={copyBtn3Ref} onClick={copyCodeEx3}></button>
+      </div>
       <p>The styling above is for the element that will be spinning behind everything. The element containing this spinning element, the 'border-effect-box', is given a style of 'overflow: hidden'. Therefore, we will only see the spinning element inside the borders of the 'border-effect-box'.</p>
       <div className="border-ex-2">
         <p>Output:</p>
         <BorderEffect />
       </div>
       <p>We then have a simple animation we add using the CSS at-rule statement '@keyframes'.</p>
-      <pre className="html-example">
-        {`@keyframes rotate {
-    to {
-      transform: rotate(360deg);
-    }
-}`}
-      </pre>
+      <div className="html-example-container">
+        <pre className="html-example">{codeEx4}</pre>
+        <button className="copy-code-btn copy-btn-4" ref={copyBtn4Ref} onClick={copyCodeEx4}></button>
+      </div>
       <div className="border-ex-3">
         <p>Output:</p>
         <BorderEffect />
       </div>
       <p>All that's left is to add the other pseudo-class for the box that will be on top of everything.</p>
-      <pre className="html-example">
-        {`.border-effect-box::after {
-    content: "Learn how to add border effects";
-    width: 208px;
-    height: 84px;
-    position: relative;
-    top: 2px;
-    left: 2px;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    font: 20px sans-serif;
-    color: white;
-    letter-spacing: 2px;
-    text-align: center;
-    background: radial-gradient(blue, black);       
-}`}
-      </pre>
+      <div className="html-example-container">
+        <pre className="html-example">{codeEx5}</pre>
+        <button className="copy-code-btn copy-btn-5" ref={copyBtn5Ref} onClick={copyCodeEx5}></button>
+      </div>
       <div className="border-ex-complete">
         <p>Output:</p>
         <BorderEffect />
