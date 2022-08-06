6b7cab3b41a971d2ff4c3addbb250b83602c7871
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-08-06 11:24:19 -0600

    css page update

--- src/components/ball-scene/BallScene.js
+++ src/components/ball-scene/BallScene.js
@@ -2,27 +2,14 @@ import "./ball-scene.css";
 
 export default function BallScene() {
   return (
-    <div className="scene-container">
-      <div className="scene">
+    <div className="ball-scene-container">
+      <div className="ball-scene">
         <div className="ball"></div>
         <div className="cube">
           <div className="front"></div>
-          <div className="back"></div>
           <div className="left"></div>
-          <div className="right"></div>
           <div className="top"></div>
           <div className="ball-shadow"></div>
-          <div className="bottom"></div>
-        </div>
-        <div className="scene-ball-2"></div>
-        <div className="scene-cube-2">
-          <div className="front"></div>
-          <div className="back"></div>
-          <div className="left"></div>
-          <div className="right"></div>
-          <div className="top"></div>
-          <div className="ball-shadow"></div>
-          <div className="bottom"></div>
         </div>
         <div className="floor"></div>
       </div>
