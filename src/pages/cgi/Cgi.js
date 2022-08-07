5205699963b4671579c6715fa4a9efbc5310fbd4
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-08-07 16:55:45 -0600

    three js testing

--- src/pages/cgi/Cgi.js
+++ src/pages/cgi/Cgi.js
@@ -15,6 +15,12 @@ export default function Cgi() {
         <h2>Four Horsemen 2/4 Pestilence and Famine</h2>
         <img className="cgi-model-image" src={horsemen2}></img>
         <Link className="cgi-model-link" to={"/cgi/cgi-models/horsemen-model-2"}>Press here to view in 3-D</Link>
+        <p>This dark yet beautiful model was created by Spinnee.</p>
+            <p>"Four Horsemen (2/4): Pestilence and Famine" (https://skfb.ly/o9VYO) by Spinnee
+                is licensed under
+                Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).</p>
+            <p>Below you will find a direct link to Sketchfab where you can view and download this model for FREE.</p>
+            <a className="sketchfab-link" href="https://sketchfab.com" target="_blank">www.Sketchfab.com</a>
       </div>
     </div>
   );
--- src/pages/cgi/cgi-models/HorsemenModel2.js
+++ src/pages/cgi/cgi-models/HorsemenModel2.js
@@ -1,3 +1,31 @@
-export default function HorsemenModel2(){
-    return <h1>Four Horsemen Model</h1>
+import React, { Suspense } from "react";
+import BackBtn from "../../css/components/BackBtn";
+import { Canvas } from "@react-three/fiber";
+import Box from "../cgi-models/cgi-components/Box";
+import Sphere from "../cgi-models/cgi-components/AnimatedSphere"
+import { OrbitControls } from "@react-three/drei";
+
+export default function HorsemenModel2() {
+  return (
+    <div className="cgi-model-section">
+      <BackBtn />
+      <h1>Four Horsemen Model</h1>
+      <Canvas className="horsemen-2-canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
+      <Canvas className="horsemen-2-canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Sphere />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
