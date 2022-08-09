caa55ab70a5e4254f51363a29a228f01b1e7ab4a
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-08-08 19:18:11 -0600

    gltf model testing 2

--- src/pages/cgi/cgi-models/HorsemenModel2.js
+++ src/pages/cgi/cgi-models/HorsemenModel2.js
@@ -2,7 +2,8 @@ import React, { Suspense } from "react";
 import BackBtn from "../../css/components/BackBtn";
 import { Canvas } from "@react-three/fiber";
 import Box from "../cgi-models/cgi-components/Box";
-import Sphere from "../cgi-models/cgi-components/AnimatedSphere"
+import Sphere from "../cgi-models/cgi-components/AnimatedSphere";
+import Horsemen2 from './cgi-components/Horsemen2'
 import { OrbitControls } from "@react-three/drei";
 
 export default function HorsemenModel2() {
@@ -24,6 +25,14 @@ export default function HorsemenModel2() {
         <directionalLight position={[-2, 5, 2]} intensity={1} />
         <Suspense fallback={null}>
           <Sphere />
+        </Suspense>
+      </Canvas>
+      <Canvas className="horsemen-2-canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Horsemen2 />
         </Suspense>
       </Canvas>
     </div>
