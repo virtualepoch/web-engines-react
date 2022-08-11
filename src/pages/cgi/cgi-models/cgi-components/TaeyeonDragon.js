8a3e1d9eb440e866b0aa228015060bb6cfb8f1f8
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-08-10 19:14:34 -0600

    gltf testing

--- src/pages/cgi/cgi-models/cgi-components/TaeyeonDragon.js
+++ src/pages/cgi/cgi-models/cgi-components/TaeyeonDragon.js
@@ -12,17 +12,17 @@ import { useGLTF } from '@react-three/drei'
 export default function Model(props) {
   const { nodes, materials } = useGLTF('/taeyeon_dragon.gltf')
   return (
-    <group {...props} dispose={null} scale={0.05}>
-      <group rotation={[-Math.PI / 2, 0, -1]}>
+    <group {...props} dispose={null} scale={0.05} position={[-0.3,-1,1]}>
+      <group rotation={[-Math.PI / 2, 0, -0.6]}>
         <group rotation={[Math.PI / 2, 0, 0]}>
           <group position={[5.43, 3.22, 10.93]} rotation={[-1.64, -0.08, -2.3]} scale={[18.26, 18.37, 18.02]}>
             <mesh geometry={nodes.Abdomen1_Skin_0.geometry} material={materials.Skin} />
             <mesh geometry={nodes.Abdomen1_Arms_0.geometry} material={materials.Arms} />
             <mesh geometry={nodes.Abdomen1_Tongue_0.geometry} material={materials.Tongue} />
-            <mesh geometry={nodes.Abdomen1_Fingernails_0.geometry} material={materials.Fingernails} />
-            <mesh geometry={nodes.Abdomen1_Claws_0.geometry} material={materials.Claws} />
-            <mesh geometry={nodes.Abdomen1_SkinWing_0.geometry} material={materials.SkinWing} />
-            <mesh geometry={nodes.Abdomen1_WingMembrane_0.geometry} material={materials.WingMembrane} />
+            {/* <mesh geometry={nodes.Abdomen1_Fingernails_0.geometry} material={materials.Fingernails} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_Claws_0.geometry} material={materials.Claws} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_SkinWing_0.geometry} material={materials.SkinWing} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_WingMembrane_0.geometry} material={materials.WingMembrane} /> */}
             <mesh geometry={nodes.Abdomen1_Sclera_0.geometry} material={materials.Sclera} />
             <mesh geometry={nodes.Abdomen1_Cornea_0.geometry} material={materials.Cornea} />
             <mesh geometry={nodes.Abdomen1_Pupils_0.geometry} material={materials.Pupils} />
@@ -33,9 +33,9 @@ export default function Model(props) {
             <mesh geometry={nodes.Abdomen1_Pupil_0.geometry} material={materials.Pupil} />
             <mesh geometry={nodes.Abdomen1_Torso_0.geometry} material={materials.Torso} />
             <mesh geometry={nodes.Abdomen1_Body_Cloth_0.geometry} material={materials.Body_Cloth} />
-            <mesh geometry={nodes.Abdomen1_Body_Edge_0.geometry} material={materials.Body_Edge} />
-            <mesh geometry={nodes.Abdomen1_Arm_Cloth_0.geometry} material={materials.Arm_Cloth} />
-            <mesh geometry={nodes.Abdomen1_Arm_Edge_0.geometry} material={materials.Arm_Edge} />
+            {/* <mesh geometry={nodes.Abdomen1_Body_Edge_0.geometry} material={materials.Body_Edge} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_Arm_Cloth_0.geometry} material={materials.Arm_Cloth} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_Arm_Edge_0.geometry} material={materials.Arm_Edge} /> */}
             <mesh geometry={nodes.Abdomen1_Side1_0.geometry} material={materials.Side1} />
             <mesh geometry={nodes.Abdomen1_Side2_0.geometry} material={materials.Side2} />
             <mesh geometry={nodes.Abdomen1_Back_0.geometry} material={materials.Back} />
@@ -44,9 +44,9 @@ export default function Model(props) {
             <mesh geometry={nodes.Abdomen1_Bangs2_0.geometry} material={materials.Bangs2} />
             <mesh geometry={nodes.Abdomen1_Front1_0.geometry} material={materials.Front1} />
             <mesh geometry={nodes.Abdomen1_Legs_0.geometry} material={materials.Legs} />
-            <mesh geometry={nodes.Abdomen1_Thigh_Cloth_0.geometry} material={materials.Thigh_Cloth} />
-            <mesh geometry={nodes.Abdomen1_Thigh_Edge_0.geometry} material={materials.Thigh_Edge} />
-            <mesh geometry={nodes.Abdomen1_WingClaws_0.geometry} material={materials.WingClaws} />
+            {/* <mesh geometry={nodes.Abdomen1_Thigh_Cloth_0.geometry} material={materials.Thigh_Cloth} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_Thigh_Edge_0.geometry} material={materials.Thigh_Edge} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_WingClaws_0.geometry} material={materials.WingClaws} /> */}
             <mesh geometry={nodes.Abdomen1_Mouth_0.geometry} material={materials.Mouth} />
             <mesh geometry={nodes.Abdomen1_Toenails_0.geometry} material={materials.Toenails} />
             <mesh geometry={nodes.Abdomen1_Bangs1_0.geometry} material={materials.Bangs1} />
@@ -62,15 +62,15 @@ export default function Model(props) {
             <mesh geometry={nodes.Abdomen1_Horns_0.geometry} material={materials.Horns} />
             <mesh geometry={nodes.Abdomen1_TeethUpper_0.geometry} material={materials.TeethUpper} />
             <mesh geometry={nodes.Abdomen1_MouthInner_0.geometry} material={materials.MouthInner} />
-            <mesh geometry={nodes.Abdomen1_Thong_0.geometry} material={materials.Thong} />
+            {/* <mesh geometry={nodes.Abdomen1_Thong_0.geometry} material={materials.Thong} /> */}
             <mesh geometry={nodes.Abdomen1_Earring_Dragonia_Earring_R_0.geometry} material={materials.Earring_Dragonia_Earring_R} />
             <mesh geometry={nodes.Abdomen1_Gem_Dragonia_Earring_R_0.geometry} material={materials.Gem_Dragonia_Earring_R} />
             <mesh geometry={nodes.Abdomen1_Body_0.geometry} material={materials.Body} />
             <mesh geometry={nodes.Abdomen1_Wing_0.geometry} material={materials.Wing} />
             <mesh geometry={nodes.Abdomen1_Eyes_0.geometry} material={materials.Eyes} />
             <mesh geometry={nodes.Abdomen1_Teeth_0.geometry} material={materials.Teeth} />
-            <mesh geometry={nodes.Abdomen1_Shin_Cloth_0.geometry} material={materials.Shin_Cloth} />
-            <mesh geometry={nodes.Abdomen1_Shin_Edge_0.geometry} material={materials.Shin_Edge} />
+            {/* <mesh geometry={nodes.Abdomen1_Shin_Cloth_0.geometry} material={materials.Shin_Cloth} /> */}
+            {/* <mesh geometry={nodes.Abdomen1_Shin_Edge_0.geometry} material={materials.Shin_Edge} /> */}
             <mesh geometry={nodes.Abdomen1_TeethLower_0.geometry} material={materials.TeethLower} />
           </group>
         </group>
