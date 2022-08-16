fbceccc4ef8db741f7bdaf105cf56dff19c08681
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-08-15 21:20:24 -0600

    custom search icon & minor edits

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -3,7 +3,7 @@ import "../search-bar/search-bar.css";
 export default function SearchBar() {
   return (
     <form className="search-bar-container">
-      <input type={"text"} className="search-bar-input" placeholder="Search our site..." aria-label="search"/>
+      <input type={"text"} className="search-bar-input" placeholder="Search our site..." aria-label="search" />
       <button className="search-btn search-bar-submit-btn" aria-label="submit-search"></button>
     </form>
   );
