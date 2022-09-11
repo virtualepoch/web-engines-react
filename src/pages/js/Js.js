0e82262d9107e503dccd1d1be1c971ee090a3862
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-09-11 13:51:23 -0600

    transition testing

--- src/pages/js/Js.js
+++ src/pages/js/Js.js
@@ -6,20 +6,26 @@ export default function Js() {
   const heading = useRef(null);
   const textContainer1 = useRef(null);
   const textContainer2 = useRef(null);
+  const textContainer3 = useRef(null);
   const textBox1 = useRef(null);
   const textBox2 = useRef(null);
+  const textBox3 = useRef(null);
 
   useEffect(() => {
     heading.current.classList.add("load");
     textContainer1.current.classList.add("load");
     textContainer2.current.classList.add("load");
+    textContainer3.current.classList.add("load");
 
     const phrase1 = "Hello";
-    const phrase2 = "Universe";
+    const phrase2 = "Player 1";
+    const phrase3 = "ARE YOU READY PLAYER ONE?";
     let i = 0;
     let j = 0;
+    let k = 0;
     let text1 = [];
     let text2 = [];
+    let text3 = [];
 
     function typePhrase1() {
       if (i < phrase1.length) {
@@ -27,11 +33,11 @@ export default function Js() {
         i++;
       }
       textBox1.current.innerHTML = text1.join("");
-      setTimeout(typePhrase1, 300);
+      setTimeout(typePhrase1, 100);
     }
     setTimeout(function () {
       typePhrase1();
-    }, 2250);
+    }, 2600);
 
     function typePhrase2() {
       if (j < phrase2.length) {
@@ -39,11 +45,22 @@ export default function Js() {
         j++;
       }
       textBox2.current.innerHTML = text2.join("");
-      setTimeout(typePhrase2, 300);
+      setTimeout(typePhrase2, 100);
     }
     setTimeout(function () {
       typePhrase2();
-    }, 4250);
+    }, 3600);
+    function typePhrase3() {
+      if (j < phrase3.length) {
+        text3.push(phrase3[k]);
+        k++;
+      }
+      textBox3.current.innerHTML = text3.join("");
+      setTimeout(typePhrase3, 100);
+    }
+    setTimeout(function () {
+      typePhrase3();
+    }, 4850);
   }, []);
 
   return (
@@ -51,12 +68,15 @@ export default function Js() {
       <h1 className="heading" ref={heading}>
         JavaScript Tutorials
       </h1>
-      <p className="hero-text">Let's add some functionality to our web apps with JavaScript!</p>
+      <p className="sub-heading hero-text">Let's add some functionality to our web apps with JavaScript!</p>
       <div className="typed-text-box text-box-1" ref={textContainer1}>
         <h2 className="typed-text" ref={textBox1}></h2>
       </div>
       <div className="typed-text-box text-box-2" ref={textContainer2}>
         <h2 className="typed-text" ref={textBox2}></h2>
+      </div>
+      <div className="typed-text-box text-box-1 text-box-3" ref={textContainer3}>
+        <h2 className="typed-text" ref={textBox3}></h2>
       </div>
       <RainCanvas />
     </div>
