435575c05f9879461256b960a2101a757166314c
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-10-18 13:06:47 -0600

    Css page heading update

--- src/components/pyramid-scene/PyramidScene.js
+++ src/components/pyramid-scene/PyramidScene.js
@@ -5,15 +5,14 @@ export default function PyramicScene() {
   return (
     <div className="pyramid-container">
       <div className="pyramid">
-        <div className="front"></div>
+        <div className="front"><div className="logo"></div></div>
         <div className="front small"></div>
         <div className="right"></div>
         <div className="right small"></div>
-        <div className="back"></div>
+        <div className="back"><div className="logo"></div></div>
         <div className="back small"></div>
         <div className="left"></div>
         <div className="left small"></div>
-        <div className="logo"></div>
         <div className="bottom"></div>
         <div className="bottom shadow"></div>
       </div>
