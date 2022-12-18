5fd4c8e39c2b6e55531b29628233714fe8be6889
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-12-18 12:48:03 -0700

    TodoPage.js style testing

--- src/components/SectionDivider.js
+++ src/components/SectionDivider.js
@@ -1,7 +1,10 @@
+import { Autoplay } from "swiper";
+
 export function SectionDivider() {
   const sectionDivider = {
     width: "100%",
     height: "20px",
+    margin: "50px auto",
     clipPath: "polygon(5px 0, 10px 40%, calc(100% - 10px) 40%, calc(100% - 5px) 0, 100% 50%, calc(100% - 5px) 100%, calc(100% - 10px) 60%, 10px 60%, 5px 100%, 0 50%)",
     background: "linear-gradient(90deg, aqua, blue, aqua)",
   };
